039b78301343bd88d41a4024b9607f17fef3e6e2
Đỗ Thanh Long <leodo310503@gmail.com>
2026-03-10 01:34:27 +0700

    create project and add User enity

---
+++ src/main/java/com/leon/jobportal/entity/Role.java
@@ -0,0 +1,7 @@
+package com.leon.jobportal.entity;
+
+public enum Role {
+    JOBSEEKER,
+    EMPLOYER,
+    ADMIN
+}
---
+++ src/main/java/com/leon/jobportal/entity/User.java
@@ -0,0 +1,27 @@
+package com.leon.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.leon.jobportal.entity.Role;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // JOBSEEKER, EMPLOYER, ADMIN
+}
